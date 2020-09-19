8fcec3abdd8a995aa76151249d232999865b2b76
Daniel Halverson <daniel.halverson@hotmail.com>
2020-09-19 14:13:26 -0600

    Adds discount model testing

---
+++ spec/models/discount_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Discount do
+  describe 'Relationships' do
+    it {should belong_to :merchant}
+
+  end
+
+  describe 'Validations' do
+    it { should validate_numericality_of :discount }
+    it { should validate_numericality_of :item_quantity }
+
+  end
+end
