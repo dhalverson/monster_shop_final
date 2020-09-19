820e81359f3461d8ea8a3fbd2ef70d1d3276b259
Daniel Halverson <daniel.halverson@hotmail.com>
2020-09-19 14:14:21 -0600

    Updates validation tests

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,7 +1,7 @@
 class Discount < ApplicationRecord
   belongs_to :merchant
 
-  validates_inclusion_of :discount, :in 1..99
-  validates_numericality_of :item_quantity, only_integer: true
+  validates_numericality_of :discount
+  validates_numericality_of :item_quantity
 
 end
