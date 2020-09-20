6887850312501e9cd4dfa6e352aec85cae3ef90c
Daniel Halverson <daniel.halverson@hotmail.com>
2020-09-20 11:14:28 -0600

    Adds discount show routing

--- config/routes.rb
+++ config/routes.rb
@@ -40,7 +40,7 @@ Rails.application.routes.draw do
     put '/items/:id/change_status', to: 'items#change_status'
     get '/orders/:id/fulfill/:order_item_id', to: 'orders#fulfill'
     get '/discounts', to: 'discounts#index'
-    get '/discounts', to: 'discounts#show'
+    get '/discounts/:id', to: 'discounts#show'
   end
 
   namespace :admin do
