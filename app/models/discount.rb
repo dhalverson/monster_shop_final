1a2a84e8f9007f6db5705b6f3333e3723c1b1e24
Daniel Halverson <daniel.halverson@hotmail.com>
2020-09-21 14:52:30 -0600

    Adds validations to model

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,6 +1,8 @@
 class Discount < ApplicationRecord
   belongs_to :merchant
 
+  validates_presence_of :discount
+  validates_presence_of :item_quantity
   validates_numericality_of :discount
   validates_numericality_of :item_quantity
 
