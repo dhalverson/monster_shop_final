61986629e13028a12e05390a87d132a1973fa37c
Daniel Halverson <daniel.halverson@hotmail.com>
2020-09-21 14:52:44 -0600

    Adds actions to discounts controller

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -9,4 +9,25 @@ class Merchant::DiscountsController < Merchant::BaseController
     @discount = Discount.find(params[:id])
   end
 
+  def edit
+    @discount = Discount.find(params[:id])
+  end
+
+  def update
+    @discount = Discount.find(params[:id])
+    @discount.update(discount_params)
+    if @discount.save
+      redirect_to "/merchant/discounts/#{@discount.id}"
+    else
+      flash[:error] = @discount.errors.full_messages.to_sentence
+      render :edit
+    end
+  end
+
+  private
+
+  def discount_params
+    params.permit(:discount, :item_quantity)
+  end
+
 end
