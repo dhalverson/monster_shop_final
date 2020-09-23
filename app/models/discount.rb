b1a281b543eb820236bf92a95a314afb15e96377
Daniel Halverson <daniel.halverson@hotmail.com>
2020-09-22 19:52:22 -0600

    Formats validations

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,9 +1,7 @@
 class Discount < ApplicationRecord
   belongs_to :merchant
 
-  validates_presence_of :discount
-  validates_presence_of :item_quantity
-  validates_numericality_of :discount
-  validates_numericality_of :item_quantity
+  validates_presence_of :discount, :item_quantity
+  validates_numericality_of :discount, :item_quantity
 
 end
