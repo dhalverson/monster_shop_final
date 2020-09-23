202cbf54f1a0455c69a13cd02bd08a54e3558219
Daniel Halverson <daniel.halverson@hotmail.com>
2020-09-22 19:52:39 -0600

    Updates cart models for discounts

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -24,23 +24,41 @@ class Cart
     end
   end
 
-  def grand_total
-    grand_total = 0.0
-    @contents.each do |item_id, quantity|
-      grand_total += Item.find(item_id).price * quantity
-    end
-    grand_total
-  end
-
   def count_of(item_id)
     @contents[item_id.to_s]
   end
 
-  def subtotal_of(item_id)
-    @contents[item_id.to_s] * Item.find(item_id).price
-  end
-
   def limit_reached?(item_id)
     count_of(item_id) == Item.find(item_id).inventory
+  end
+
+  def subtotal_of(item_id)
+    item = Item.find(item_id)
+    @contents[item_id.to_s] * item.price
+  end
+
+  def discounted_subtotal_of(item_id)
+    item = Item.find(item_id)
+    item_discount_percent = (1 - (item.highest_discount(count_of(item_id)).discount) / 100.0)
+    count_of(item_id) * (item.price * item_discount_percent)
+  end
+
+  def has_discount?(item_id, merchant_discounts)
+    merchant_discounts.any? do |discount|
+      (count_of(item_id)) >= (discount.item_quantity)
+    end
+  end
+
+  def grand_total
+    grand_total = 0.0
+    @contents.each do |item_id, quantity|
+      item = Item.find(item_id)
+      if has_discount?(item_id, item.merchant.discounts)
+        grand_total += discounted_subtotal_of(item_id)
+      else
+        grand_total += subtotal_of(item_id)
+      end
+    end
+    grand_total
   end
 end
